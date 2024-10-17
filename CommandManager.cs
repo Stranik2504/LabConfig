6a0f4a71af4e7c59e96195ebe4ad6004eca3d620
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2024-10-17 19:07:41 +0300

    End work with interface, start work with commands

---
+++ CommandManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace LabConfig;
+
+public static class CommandManager
+{
+    private const int CountUsers = 1;
+    
+    public static event Action Close;
+    public static event Action<string> ChangeDir;
+
+    private static DateTime _startTime;
+
+    static CommandManager()
+    {
+        _startTime = DateTime.Now;
+    }
+    
+    public static string Execute(string command)
+    {
+        if (command == "exit")
+        {
+            Close?.Invoke();
+            return string.Empty;
+        }
+        
+        if (command.StartsWith("uptime"))
+        {
+            
+        }
+        
+        return "command not found: " + command;
+    }
+
+    public static string UptimeCommand(string command)
+    {
+        
+    }
+
+    private static string UptimeCommon(string command)
+    {
+        var uptime = DateTime.Now - _startTime;
+        var timeup = string.Empty;
+            
+        if (uptime.Seconds > 0)
+            timeup = uptime.Seconds + " sec";
+            
+        if (uptime.Minutes > 0)
+            timeup = uptime.Minutes + " min" + (string.IsNullOrWhiteSpace(timeup) ? "" : " ") + timeup;
+            
+        if (uptime.Hours > 0)
+            timeup = uptime.Hours + " hours" + (string.IsNullOrWhiteSpace(timeup) ? "" : " ") + timeup;
+            
+        if (uptime.Days > 0)
+            timeup = uptime.Days + " days" + (string.IsNullOrWhiteSpace(timeup) ? "" : " ") + timeup;
+            
+        return 
+            DateTime.Now.ToString("HH:mm:ss") + 
+            " up " + timeup + "," +
+            $" {CountUsers} user, " + 
+            "load average: 0.00, 0.00, 0.00";
+    }
+    
+    private static string UptimePretty(string command)
+    {
+        var uptime = DateTime.Now - _startTime;
+        var timeup = string.Empty;
+            
+        if (uptime.Seconds > 0)
+            timeup = uptime.Seconds + " sec";
+            
+        if (uptime.Minutes > 0)
+            timeup = uptime.Minutes + " min" + (string.IsNullOrWhiteSpace(timeup) ? "" : " ") + timeup;
+            
+        if (uptime.Hours > 0)
+            timeup = uptime.Hours + " hours" + (string.IsNullOrWhiteSpace(timeup) ? "" : " ") + timeup;
+            
+        if (uptime.Days > 0)
+            timeup = uptime.Days + " days" + (string.IsNullOrWhiteSpace(timeup) ? "" : " ") + timeup;
+            
+        return 
+            "up " + timeup;
+    }
+
+    private static string UptimeSince(string command)
+    {
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LabConfig
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            var dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += (sender, e) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    LTime.Content = "🕘 " + DateTime.Now.ToString("HH:mm:ss");
+                    LDate.Content = "📅 " + DateTime.Now.ToString("yyyy-MM-dd");
+                }, DispatcherPriority.Background);
+            };
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+            
+            TbInput.Focusable = true;
+            TbInput.Focus();
+            
+            RtbHistory.Document.Blocks.Clear();
+            
+            CommandManager.Close += Close;
+            CommandManager.ChangeDir += path => { LPath.Content = path; };
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            RtbHistory.MaxHeight = DockAllText.ActualHeight - DockInput.ActualHeight;
+            RtbHistory.ScrollToEnd();
+        }
+
+        private void MainWindow_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            RtbHistory.MaxHeight = DockAllText.ActualHeight - DockInput.ActualHeight;
+        }
+
+        private void RtbHistory_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is not RichTextBox richTextBox) return;
+
+            richTextBox.Visibility = richTextBox.Document.Blocks.Count > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void UIElement_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender is not TextBox textBox) return;
+            if (e.Key != Key.Enter) return;
+
+            var command = textBox.Text;
+            textBox.Text = string.Empty;
+            
+            var result = CommandManager.Execute(command);
+            if (string.IsNullOrEmpty(result)) return;
+            
+            if (RtbHistory.Document.Blocks.Count > 0)
+                RtbHistory.AppendText(Environment.NewLine);
+            
+            RtbHistory.AppendText(result);
+        }
+    }
+}
